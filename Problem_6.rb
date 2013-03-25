981bdd10bb2c26599807d1038eef20f75050ea6d
Naoto Inoue <dorapocket22@gmail.com>
2013-03-25 19:45:03 +0900

    Settled

---
+++ Problem_6.rb
@@ -0,0 +1,11 @@
+# Sum square difference
+
+# sum of the squares
+# 1/6 n (n + 1) (2n + 1)
+a = (100 * (100 + 1) * (2 * 100 + 1)) / 6
+
+# square of the sum
+# {1/2 n (n + 1)}^2
+b = (100 * (100 + 1) / 2) ** 2
+
+puts (b - a)
